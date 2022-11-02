d83d46d83f79e333f9bc07bcd2900b30f1abe68e
GlebPetrovsky <gleb.piatrouski@gmail.com>
2022-11-02 17:10:39 +0300

    Different tasks

---
+++ 05-hometask/app.js
@@ -0,0 +1,50 @@
+//Task - 1
+
+//$str = '468249';
+//if ($str[0] + $str[1] + $str[2] == $str[3] + $str[4] + $str[5])
+//console.log('Да');
+//else console.log('Нет');
+//console.log('<br/>');
+
+//Task - 2
+
+//for (let n = 1000; n <= 50; n++) {
+//alert(n);
+//}
+
+//Task - 3
+
+//function  average(arr)
+//{
+//if(arr.length === 0)
+//return 0;
+
+//let sum = 0;
+
+//for(let i = 0; i < arr.length; i++) {
+//sum += arr[i];
+//}
+//return sum / arr.length;
+//}
+
+//let k = [1, 2, 3, 4, 5, 6, 7];
+//console.log(average(k));
+
+//Task - 4
+
+//let a = [1, 2, 3, 4, 5];
+//a.splice(3, 0, 'a', 'b', 'c');
+//console.log(a);
+
+//Task - 5
+
+//let a = [1, 2, 3, 4, 5];
+//a.splice(1, 0, 'a', 'b');
+//a.splice(6, 0, 'c');
+//a.splice(8, 0, 'e');
+//console.log(a);
+
+//Task-6
+
+//let a = [3, 4, 1, 2, 5];
+//console.log(a.sort());
